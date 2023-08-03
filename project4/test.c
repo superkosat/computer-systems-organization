505bcf6f9de8c33b29a1bc5305619a1c843a54ac
superkosat <36095695+superkosat@users.noreply.github.com>
2023-08-03 17:13:18 -0400

    completed project4 code
    completed project4 code pending more extensive testing

--- project4/test.c
+++ project4/test.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+//method signatures for program's constituent functions
+void searchTLB(int TLBT, int TLBI, int cacheIndex, char **physicalPageNumber, char **CachedArray);
+void searchPageTable(int VPN, int cacheIndex, char **physicalPageNumber, char **CachedArray);
+void searchCache(int offset, int cacheIndex, char **physicalPageNumber, char **CachedArray);
+
+//main method assumes the input file is named "Project4Input.txt" and executes accordingly
 int main() {
     FILE *fileptr;
     fileptr = fopen("Project4Input.txt", "r");
@@ -35,7 +41,6 @@ int main() {
         //reads string as hex and stores in an int
         int intValue;
         sscanf(input, "%x", &intValue);
-        printf("Base 10 value: %d\n", intValue);
 
         //extracts components of virtual address by bits
         int mask = 0xFF << 6;
@@ -54,19 +59,41 @@ int main() {
         int offset = (intValue & mask);
         printf("VPO: %x\n", offset);
 
+        //searches tlb entries matching tlb tag and tlb index
+        char *physicalPageNumber = "MISS";
+        searchTLB(TLBT, TLBI, cacheIndex, &physicalPageNumber, &(*CachedArray));
+        if (strcmp(physicalPageNumber, "MISS") == 0) {
+            //search page table and retrieve physical page number
+            printf("Not in TLB\n");
+            searchPageTable(VPN, cacheIndex, &physicalPageNumber, &(*CachedArray));
+            if (strcmp(physicalPageNumber, "MISS") == 0) {
+                printf("Not in Page Table\n");
+                printf("Cannot be determined\n");
+            } else {
+                //combine relevant bits with offset. then extract tag, index, offset, and search cache
+                printf("In Page Table\n");
+                printf("%s", physicalPageNumber);
+                searchCache(offset, cacheIndex, &physicalPageNumber, &(*CachedArray));
+                
+            }
+
+        } else {
+            //combine relevant ppn bits with offset. then extract tag, index, and offset, search cache
+            printf("In TLB\n");
+            printf("%s", physicalPageNumber);
+            searchCache(offset, cacheIndex, &physicalPageNumber, &(*CachedArray));
+        }
 
 
         printf("Enter an address: ");
         scanf("%s", input);
     }
 
+    /*
     //Print elements of cached array
     for (int i = 0; i < cacheIndex; i++) {
         printf("Element %d: %s\n", i, CachedArray[i]);
-        if (strcmp(CachedArray[i], "TLB") == 0) {
-            printf("TLB\n");
-        }
-    }
+    }*/
 
     for (int i = 0; i < cacheIndex; i++) {
         free(CachedArray[i]);
@@ -75,8 +102,59 @@ int main() {
 
     fclose(fileptr);
 
-    
-
     return 0;
 }
 
+//searches tlb for matching tlb tag and tlb index and modifies physical page number accordingly
+void searchTLB(int TLBT, int TLBI, int cacheIndex, char **physicalPageNumber, char **CachedArray) {
+    for (int i = 0; i < cacheIndex; i++) {
+        if (strcmp(CachedArray[i], "TLB") == 0) {
+            int tag;
+            int index;
+            sscanf(CachedArray[i + 1], "%x", &tag);
+            sscanf(CachedArray[i + 2], "%x", &index);
+            if (tag == TLBT && index == TLBI) {
+                *physicalPageNumber = CachedArray[i + 3];
+            }
+        }
+    }
+    return;
+}
+
+//searches page table for vpn entry and modifies physical page number accordingly
+void searchPageTable(int VPN, int cacheIndex, char **physicalPageNumber, char **CachedArray) {
+    for (int i = 0; i < cacheIndex; i++) {
+        if (strcmp(CachedArray[i], "Page") == 0) {
+            int VPNEntry;
+            sscanf(CachedArray[i + 1], "%x", &VPNEntry);
+            if (VPN == VPNEntry) {
+                *physicalPageNumber = CachedArray[i + 2];
+            }
+        }
+    }
+    return;
+}
+
+void searchCache(int offset, int cacheIndex, char **physicalPageNumber, char **CachedArray) {
+    //extract tag, index, and offset from physical address
+    int tagBits;
+    sscanf(*physicalPageNumber, "%x", &tagBits);
+    int offsetBits = offset & 0x03;
+    int indexBits = offset & 0x3c;
+
+    //search cache and return byte if present
+    for (int i = 0; i < cacheIndex; i++) {
+        if (strcmp(CachedArray[i], "Cache") == 0) {
+            int tagEntry;
+            int indexEntry;
+            sscanf(CachedArray[i + 1], "%x", &tagEntry);
+            sscanf(CachedArray[i + 2], "%x", &indexEntry);
+            if (tagBits == tagEntry && indexBits == indexEntry) {
+                printf("Byte returned: %s\n", CachedArray[i + offsetBits + 3]);
+                return;
+            }
+        }
+    }
+    printf("Not in Cache\n");
+    printf("Cannot be determined\n");
+}
