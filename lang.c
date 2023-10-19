3f598549ef983e0bc7de658fe018bd5ff782db34
superkosat <36095695+superkosat@users.noreply.github.com>
2023-10-19 15:45:31 +0800

    old changes
    committed old untracked changes

--- lang.c
+++ lang.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void printLine();
 
 typedef enum type {
     VAR,
     NUM,
     OP,
-    ASSIGN
+    ASSIGN,
+    PRINT
 } type;
 
 typedef struct token {
@@ -15,8 +19,21 @@ typedef struct token {
 
 int main(int argc, char **argv) {
     FILE *src;
-    src = fopen((*argv)[1], "r");
-    while () {
+    src = fopen((argv)[1], "r");
+    char *mode = argv[2];
 
+    printf("%s\n", mode);
+    char *str = (char*)malloc(20 * sizeof(char));
+    printf("%s", fgets(str, 1024, src));
+
+    
+    //While has next line
+    while (fgets(str, 1024, src)) {
+        printLine(&str);
     }
+
+}
+
+void printLine(char **str) {
+    printf("%s", *str);
 }
